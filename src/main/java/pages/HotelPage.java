5d5683a38f9f68de8f70436ee407baa1706a13d5
Gekay <hifixpert@gmail.com>
2023-05-29 22:18:05 +0100

    Version2

--- src/main/java/pages/HotelPage.java
+++ src/main/java/pages/HotelPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -12,7 +13,46 @@ public class HotelPage {
 		PageFactory.initElements(driver, this);
 	}
 	
+By Language = By.xpath("//button[@id='languages']");
+By LangSele = By.xpath("//button[@id='languages']/../ul/li[9]");
+
+
+
+By ClickOnCity = By.xpath("//span[@id='select2-hotels_city-container']");
+By EnterCity = By.xpath("//input[@class= 'select2-search__field']");
+By ClickCity = By.xpath("//ul[@id ='select2-hotels_city-results']/li[1]/i");
+
+
+
+
+
+public void clickOnLanguage() {
+	driver.findElement(Language).click();
+}
+
+public void SelectLang(){
+	driver.findElement(LangSele).click();
 	
+}
+
+	
+public void ClickOnCity() {
+	driver.findElement(ClickOnCity).click();
+	
+	
+}
+
+
+public void enterCityName(String city) throws InterruptedException {
+		driver.findElement(EnterCity).sendKeys(city);
+		Thread.sleep(3000);
+	}
+
+public void clickcity() {
+	driver.findElement(ClickCity).click();
+
+}
+
 	
 	
 	
--- src/test/java/test/HotelTest.java
+++ src/test/java/test/HotelTest.java
@@ -7,39 +7,48 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import library.Constants;
+import com.assertthat.selenium_shutterbug.core.Shutterbug;
 
+import library.Constants;
+import pages.HotelPage;
 
 public class HotelTest {
 	WebDriver driver;
-	
+
 	@Parameters("Browser")
 	@BeforeMethod
 	public void before(String browser) {
-		if(browser.equalsIgnoreCase("Firefox")) {
+		if (browser.equalsIgnoreCase("Firefox")) {
 			driver = library.Browser.launch("Firefox");
-		}else if(browser.equalsIgnoreCase("Chrome")) {
+		} else if (browser.equalsIgnoreCase("Chrome")) {
 			driver = library.Browser.launch("Chrome");
-		}else if(browser.equalsIgnoreCase("Edge")) {
+		} else if (browser.equalsIgnoreCase("Edge")) {
 			driver = library.Browser.launch("Edge");
+		} else {
+			driver = library.Browser.launch("Firefox");
 		}
 		driver.get(Constants.URL);
 	}
-	
+
 	@AfterMethod
 	public void after(ITestResult result) {
-		if(ITestResult.FAILURE == result.getStatus()) {
+		if (ITestResult.FAILURE == result.getStatus()) {
 			System.out.println("The testcase that failed is:" + result.getName());
+			Shutterbug.shootPage(driver).withName(result.getName()).save();
 		}
-		
+
 	}
-	
-	@Test 
-	public void testcase1() {
-		
-	
+
+	@Test
+	public void testcase1() throws InterruptedException {
+		HotelPage city = new HotelPage(driver);
+
+		city.clickOnLanguage();
+		city.SelectLang();
+		city.ClickOnCity();
+		city.enterCityName(Constants.CITYNAME);
+		city.clickcity();
+
 	}
-	
-	
 
 }
