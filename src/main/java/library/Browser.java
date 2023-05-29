d1c03dbcfdd10d77a701e641c230e2b80efd2541
Gekay <hifixpert@gmail.com>
2023-05-29 13:30:05 +0100

    project1

---
+++ src/main/java/library/Browser.java
@@ -0,0 +1,27 @@
+package library;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Browser {
+	public static WebDriver launch(String browser) {
+		if(browser.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			return new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("Firefox")){
+			WebDriverManager.firefoxdriver().setup();
+			return new FirefoxDriver();
+		}else if(browser.equalsIgnoreCase("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			return new EdgeDriver();
+		}else {
+			WebDriverManager.edgedriver().setup();
+			return new EdgeDriver();
+		}
+	}
+
+}
